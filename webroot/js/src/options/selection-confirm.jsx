acab6158fe03dee893534b468eb863cbc455aac0
Jon Mason <jon.mason@medsci.ox.ac.uk>
2016-11-22 17:18:47 +0000

    Only show confirm content in paper if there are warnings

--- webroot/js/src/options/selection-confirm.jsx
+++ webroot/js/src/options/selection-confirm.jsx
@@ -10,6 +10,51 @@ import MultilineField from '../elements/fields/multiline-text.jsx';
 
 var SelectionConfirm = React.createClass({
     render: function() {
+        var content = <div>
+            <EditableWarning
+                instance={this.props.instance.instance}
+            />
+            
+            {(this.props.instance.instance.preference_instructions)&&
+                <p>{this.props.instance.instance.preference_instructions}</p>
+            }
+                
+            {(this.props.instance.instance.comments_per_option_instructions)&&
+                <p>{this.props.instance.instance.comments_per_option_instructions}</p>
+            }
+        
+            {(this.props.optionsSelectedPreferenceOrder.length > 0)?
+                <div style={{width: '100%'}}>
+                    {/*<p>You have chosen the following options:</p>*/}
+                    <OptionList
+                        action="confirm"
+                        handleOrderChange={this.props.optionContainerHandlers.orderChange}
+                        handleCommentsChange={this.props.optionContainerHandlers.optionCommentsChange}
+                        instance={this.props.instance.instance}
+                        optionIds={this.props.optionsSelectedPreferenceOrder}
+                        options={this.props.options}
+                        optionsSelected={this.props.optionsSelected}
+                        rankSelectsDisabled={this.props.rankSelectsDisabled}
+                        removeButton={false}
+                        useCode={this.props.choice.use_code}
+                    />
+                </div>
+            :
+                <div>No options chosen</div>
+            }
+            
+            {(this.props.instance.instance.comments_overall)?
+                <MultilineField field={{
+                    label: "Comments",
+                    instructions: this.props.instance.instance.comments_overall_instructions,
+                    name: "selection.comments",
+                    onChange: this.props.optionContainerHandlers.overallCommentsChange,
+                    section: true,
+                    value: this.props.selection.selection.comments,
+                }} />
+            :""}
+        </div>;
+    
         return (
             <div>
                 <Warnings
@@ -18,51 +63,13 @@ var SelectionConfirm = React.createClass({
                     ruleWarnings={this.props.selection.ruleWarnings}
                 />
                 
-                
-                <Paper rounded={false} zDepth={2} style={{padding: '16px', margin: '15px 0'}}>
-                    <EditableWarning
-                        instance={this.props.instance.instance}
-                    />
-                    
-                    {(this.props.instance.instance.preference_instructions)&&
-                        <p>{this.props.instance.instance.preference_instructions}</p>
-                    }
-                        
-                    {(this.props.instance.instance.comments_per_option_instructions)&&
-                        <p>{this.props.instance.instance.comments_per_option_instructions}</p>
-                    }
-                
-                    {(this.props.optionsSelectedPreferenceOrder.length > 0)?
-                        <div style={{width: '100%'}}>
-                            {/*<p>You have chosen the following options:</p>*/}
-                            <OptionList
-                                action="confirm"
-                                handleOrderChange={this.props.optionContainerHandlers.orderChange}
-                                handleCommentsChange={this.props.optionContainerHandlers.optionCommentsChange}
-                                instance={this.props.instance.instance}
-                                optionIds={this.props.optionsSelectedPreferenceOrder}
-                                options={this.props.options}
-                                optionsSelected={this.props.optionsSelected}
-                                rankSelectsDisabled={this.props.rankSelectsDisabled}
-                                removeButton={false}
-                                useCode={this.props.choice.use_code}
-                            />
-                        </div>
-                    :
-                        <div>No options chosen</div>
-                    }
-                    
-                    {(this.props.instance.instance.comments_overall)?
-                        <MultilineField field={{
-                            label: "Comments",
-                            instructions: this.props.instance.instance.comments_overall_instructions,
-                            name: "selection.comments",
-                            onChange: this.props.optionContainerHandlers.overallCommentsChange,
-                            section: true,
-                            value: this.props.selection.selection.comments,
-                        }} />
-                    :""}
-                </Paper>
+                {(this.props.selection.ruleWarnings)?
+                    <Paper rounded={false} zDepth={2} style={{padding: '16px', margin: '15px 0'}}>
+                        {content}
+                    </Paper>
+                :
+                    content
+                }
             </div>
         );
     }
