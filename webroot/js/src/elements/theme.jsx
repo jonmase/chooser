ed3098e71df681f303d299b0ddfafc12d7a77667
Jon Mason <jon.mason@medsci.ox.ac.uk>
2016-11-24 20:00:48 +0000

    Changed to Indigo theme

--- webroot/js/src/elements/theme.jsx
+++ webroot/js/src/elements/theme.jsx
@@ -1,18 +1,29 @@
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
+/*
 import {teal300, teal500, teal700, lightBlack, indigoA200, lightBlue300} from 'material-ui/styles/colors';
 
 //http://www.material-ui.com/#/customization/colors
 module.exports = getMuiTheme({
   palette: {
-    //primary1Color: indigo700,
     primary1Color: teal500,
-    //primary2Color: indigo900,
     primary2Color: teal700,
     primary3Color: lightBlack,
-    //accent1Color: indigoA400,
-    //accent1Color: indigoA200,
     accent1Color: teal300,
-    //pickerHeaderColor: indigo700,
     pickerHeaderColor: teal500,
+  }
+});
+*/
+
+import {indigoA200, indigo500, indigo700, lightBlack, lightBlue300} from 'material-ui/styles/colors';
+
+module.exports = getMuiTheme({
+  palette: {
+    primary1Color: indigo500,
+    primary2Color: indigo700,
+    primary3Color: lightBlack,
+    //accent1Color: indigoA400,
+    accent1Color: indigoA200,
+    pickerHeaderColor: indigo500,
   }
 });
