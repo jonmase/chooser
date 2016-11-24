f1c69fca2b71cd0b94b63a0feef14c2010299ce9
Jon Mason <jon.mason@medsci.ox.ac.uk>
2016-11-24 15:12:50 +0000

    Hid more/expand button in options table

--- webroot/js/src/options/option-table.jsx
+++ webroot/js/src/options/option-table.jsx
@@ -241,7 +241,7 @@ var OptionsTable = React.createClass({
                                 {this.props.action === 'edit'? 
                                     <TableHeaderColumn style={styles.actionsTableRowColumn}></TableHeaderColumn>
                                 :""}
-                                <TableHeaderColumn style={styles.actionsTableRowColumn}></TableHeaderColumn>
+                                {/*<TableHeaderColumn style={styles.actionsTableRowColumn}></TableHeaderColumn>*/}
                             </TableRow>
                         </TableHeader>
                         <TableBody 
@@ -298,13 +298,14 @@ var OptionsTable = React.createClass({
                                                 />
                                             </UnselectableCell>
                                         :""}
-                                        <UnselectableCell style={styles.actionsTableRowColumn}>
+                                        {/*<UnselectableCell style={styles.actionsTableRowColumn}>
                                             <ExpandButton
                                                 handleMore={optionTableHandlers.dialogOpen} 
                                                 id={option.id}
                                                 tooltip=""
                                             />
                                         </UnselectableCell>
+                                        */}
                                     </TableRow>
                                 );
                             }, this)}
