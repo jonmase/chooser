2450bf28c16a73d796204157a297a1bb8df0f345
Jon Mason <jon.mason@medsci.ox.ac.uk>
2017-04-07 12:14:00 +0100

    Fixed issue with checkboxes without values

--- webroot/js/src/elements/fields/checkbox.jsx
+++ webroot/js/src/elements/fields/checkbox.jsx
@@ -10,7 +10,7 @@ var CheckboxField = React.createClass({
 
         var checkboxes = field.options.map(function(option) {
             var defaultChecked = false;
-            if(typeof(field.value) !== "undefined" && typeof(field.value[option.value]) !== "undefined") {
+            if(field.value && typeof(field.value[option.value]) !== "undefined") {
                 defaultChecked = !!field.value[option.value];
             }
             
