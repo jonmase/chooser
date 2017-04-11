2c368bfb5e6eaa9d8cde0e76316fc6db36f1fed6
Jon Mason <jon.mason@medsci.ox.ac.uk>
2017-04-11 11:36:11 +0100

    Added Reset button to reset page topbar

--- webroot/js/src/reset/reset-container.jsx
+++ webroot/js/src/reset/reset-container.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 
-import Snackbar from 'material-ui/Snackbar';
+import Formsy from 'formsy-react';
+import FormsyToggle from 'formsy-material-ui/lib/FormsyToggle';
+
+import RaisedButton from 'material-ui/RaisedButton';
 
 import Container from '../elements/container.jsx';
 import TopBar from '../elements/topbar.jsx';
@@ -9,27 +12,33 @@ import AppTitle from '../elements/app-title.jsx';
 var ResetContainer = React.createClass({
     getInitialState: function () {
         return {
+            saveButtonEnabled: true,
+            saveButtonLabel: 'Reset',
         };
     },
     componentDidMount: function() {
+    },
+    
+    handleResetClick: function() {
+    
     },
     
     render: function() {
         var topbar = <TopBar 
             dashboardUrl={this.props.dashboardUrl} 
             iconLeft="menu"
-            iconRight={null}
+            iconRight={<RaisedButton 
+                //disabled={!this.state.canSubmit && this.state.saveButtonEnabled}
+                label={this.state.saveButtonLabel}
+                onTouchTap={this.handleResetClick}
+                style={{marginTop: '6px'}}
+            />}
             sections={this.props.sections} 
             title={<AppTitle subtitle={this.props.choice.name + ": Reset Choice"} />}
         />;
         
         return (
             <Container topbar={topbar} title={false}>
-                {/*<Reset
-                    handlers={settingsHandlers}
-                    instance={this.state.instance}
-                    instanceLoaded={this.state.instanceLoaded}
-                />*/}
             </Container>
         );
     }
