9a6dd6ae65a843e9a2a4fc9a6ddaf27e0744b0b9
Jon Mason <jon.mason@medsci.ox.ac.uk>
2017-04-11 11:47:42 +0100

    Created reset form

--- webroot/js/src/reset/reset-container.jsx
+++ webroot/js/src/reset/reset-container.jsx
@@ -12,6 +12,7 @@ import AppTitle from '../elements/app-title.jsx';
 var ResetContainer = React.createClass({
     getInitialState: function () {
         return {
+            canSubmit: false,
             saveButtonEnabled: true,
             saveButtonLabel: 'Reset',
         };
@@ -19,6 +20,18 @@ var ResetContainer = React.createClass({
     componentDidMount: function() {
     },
     
+    enableSubmitButton: function () {
+        this.setState({
+            canSubmit: true
+        });
+    },
+
+    disableSubmitButton: function () {
+        this.setState({
+            canSubmit: false
+        });
+    },
+
     handleResetClick: function() {
     
     },
@@ -28,7 +41,7 @@ var ResetContainer = React.createClass({
             dashboardUrl={this.props.dashboardUrl} 
             iconLeft="menu"
             iconRight={<RaisedButton 
-                //disabled={!this.state.canSubmit && this.state.saveButtonEnabled}
+                disabled={!this.state.canSubmit && this.state.saveButtonEnabled}
                 label={this.state.saveButtonLabel}
                 onTouchTap={this.handleResetClick}
                 style={{marginTop: '6px'}}
@@ -39,6 +52,36 @@ var ResetContainer = React.createClass({
         
         return (
             <Container topbar={topbar} title={false}>
+                <p>Resetting the Choice will archive the current schedule and the associated results. You will still be able to view archived the schedule and results. Any additional permissions given to users, the options form and all options will be kept.</p>
+                <p>You can also do some other 'tidying up' at this point, according to the settings you choose below. </p>
+                <Formsy.Form
+                    id="reset_form"
+                    method="POST"
+                    onValid={this.enableSubmitButton}
+                    onInvalid={this.disableSubmitButton}
+                    onValidSubmit={this.handleSubmit}
+                    noValidate={true}
+                    ref="reset"
+                >
+                    <FormsyToggle
+                        defaultToggled={false}
+                        label="Unpublish all Options? (Options will have to be published again before they are visible to viewers)"
+                        labelPosition="right"
+                        name="unpublish"
+                    />
+                    <FormsyToggle
+                        defaultToggled={true}
+                        label="Keep Choice Settings? (The Choice Settings, including instructions, preference settings, etc., will be kept, but the dates will be reset)"
+                        labelPosition="right"
+                        name="settings"
+                    />
+                    <FormsyToggle
+                        defaultToggled={true}
+                        label="Keep Rules?"
+                        labelPosition="right"
+                        name="rules"
+                    />
+                </Formsy.Form>
             </Container>
         );
     }
