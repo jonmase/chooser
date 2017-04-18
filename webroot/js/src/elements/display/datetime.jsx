b33ee5eb6f1831c12d26f4107f98aa636cfeaafe
Jon Mason <jon.mason@medsci.ox.ac.uk>
2017-04-18 10:26:54 +0100

    Fixed datetime display element value conditions

--- webroot/js/src/elements/display/datetime.jsx
+++ webroot/js/src/elements/display/datetime.jsx
@@ -4,7 +4,7 @@ var DateTimeDisplay = React.createClass({
     render: function() {
         return (
             <span>
-                {this.props.value.formatted || "Not set"}
+                {(this.props.value && this.props.value.formatted)?this.props.value.formatted:"Not set"}
             </span>
         );
     }
