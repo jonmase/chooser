3ba0351d1d7b5615a0d1b5fa20834dfc70d85b38
Jon Mason <jon.mason@medsci.ox.ac.uk>
2017-09-28 16:28:55 +0100

    Deal with missing options when displaying options list

--- webroot/js/src/options/option-list.jsx
+++ webroot/js/src/options/option-list.jsx
@@ -50,97 +50,112 @@ var OptionList = React.createClass({
                 {this.props.optionsSelectedIdsOrdered.map(function(optionId, optionIndex) {
                     var option = this.props.options[this.props.optionIndexesById[optionId]];
                     
-                    if(this.props.useCode) {
-                        var primaryText = option.code;
-                        var secondaryText = option.title;
-                        var itemHeight = '71px'
+                    if(typeof(option) === "undefined") {
+                        //If option is undefined, means that selected option is no longer in options list
+                        return (
+                            <div key={optionId}>
+                                <ListItem
+                                    disabled={true}
+                                    primaryText="Option is no longer available"
+                                    style={listItemStyle}
+                                />
+                                <Divider />
+                            </div>
+                        );
                     }
                     else {
-                        var primaryText = option.title;
-                        
-                        //Make secondary text blank...
-                        var secondaryText = "";
-                        //...unless either preference or comments field is showing, in which case make it a space, so that it is still there to pad out the list item block
-                        if(this.props.showPreferenceInputs || this.props.showCommentsFieldPerOption) {
-                            secondaryText = " ";
+                        if(this.props.useCode) {
+                            var primaryText = option.code;
+                            var secondaryText = option.title;
+                            var itemHeight = '71px'
                         }
-                        
-                        var itemHeight = '47px'
-                    }
-                    
-                    return (
-                        <div key={optionId}>
-                            {(this.props.showPreferenceInputs)?
-                                <div style={{float: 'left'}}>
-                                    {(this.props.preferenceType === 'rank')?
-                                        <DropdownField
-                                            field={{
-                                                disabled: this.props.rankSelectsDisabled,
-                                                label: false,
-                                                name: "options." + optionId + ".rank",
-                                                onChange: this.props.handleOrderChange,
-                                                options: rankDropdownOptions,
-                                                required: true,
-                                                style: {width: '40px', marginTop: '12px'},
-                                                value: optionIndex,
-                                            }}
-                                        />
-                                    :""}
-                                </div>
-                            :""}
-                            {(this.props.showPreferenceValues)?
-                                <div style={{float: 'left', width: '20px', display: 'table', height: itemHeight, tableLayout: 'fixed'}}>
-                                    <div style={{display: 'table-cell', verticalAlign: 'middle'}}>
-                                        {(this.props.preferenceType === 'rank')&&
-                                            (this.props.optionsSelectedById[optionId].rank + 1)
-                                        }
-                                    </div>
-                                </div>
-                            :""}
-                        
-                            {(this.props.showCommentsFieldPerOption)?
-                                <div style={{float: 'right', marginTop: '-8px'}}>
-                                    <TextField field={{
-                                        defaultValue: this.props.optionsSelectedById[optionId].comments,
-                                        label: "Option-specific comments",
-                                        name: "options." + optionId + ".comments",
-                                        onChange: this.props.handleCommentsChange,
-                                        section: false,
-                                    }} />
-                                </div>
-                            :""}
-                            {(this.props.showCommentsTextPerOption)?
-                                <div style={{float: 'right', width: '50%', minWidth: '200px', display: 'table', height: itemHeight, tableLayout: 'fixed', textAlign: 'right'}}>
-                                    <div style={{display: 'table-cell', verticalAlign: 'middle', overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap'}}>
-                                        {this.props.optionsSelectedById[optionId].comments}
-                                    </div>
-                                </div>
-                            :""}
+                        else {
+                            var primaryText = option.title;
                             
-                            {this.props.hiddenOptionIdField &&
-                                <HiddenField value={optionId} name={"options." + optionId + ".choices_option_id"} />
+                            //Make secondary text blank...
+                            var secondaryText = "";
+                            //...unless either preference or comments field is showing, in which case make it a space, so that it is still there to pad out the list item block
+                            if(this.props.showPreferenceInputs || this.props.showCommentsFieldPerOption) {
+                                secondaryText = " ";
                             }
                             
-                            <ListItem
-                                disabled={typeof(this.props.disabled) !== "undefined"?this.props.disabled:true}
-                                primaryText={primaryText}
-                                rightIconButton={
-                                    (this.props.removeButton)?
-                                        <IconButton
-                                            onTouchTap={() => this.props.removeHandler(optionId)}
-                                            iconClassName="material-icons"
-                                        >
-                                            close
-                                        </IconButton>
-                                    :null   //Have to give null as false value, otherwise get warning about using bool when expecting ReactElement
+                            var itemHeight = '47px'
+                        }
+                        
+                        return (
+                            <div key={optionId}>
+                                {(this.props.showPreferenceInputs)?
+                                    <div style={{float: 'left'}}>
+                                        {(this.props.preferenceType === 'rank')?
+                                            <DropdownField
+                                                field={{
+                                                    disabled: this.props.rankSelectsDisabled,
+                                                    label: false,
+                                                    name: "options." + optionId + ".rank",
+                                                    onChange: this.props.handleOrderChange,
+                                                    options: rankDropdownOptions,
+                                                    required: true,
+                                                    style: {width: '40px', marginTop: '12px'},
+                                                    value: optionIndex,
+                                                }}
+                                            />
+                                        :""}
+                                    </div>
+                                :""}
+                                {(this.props.showPreferenceValues)?
+                                    <div style={{float: 'left', width: '20px', display: 'table', height: itemHeight, tableLayout: 'fixed'}}>
+                                        <div style={{display: 'table-cell', verticalAlign: 'middle'}}>
+                                            {(this.props.preferenceType === 'rank')&&
+                                                (this.props.optionsSelectedById[optionId].rank + 1)
+                                            }
+                                        </div>
+                                    </div>
+                                :""}
+                            
+                                {(this.props.showCommentsFieldPerOption)?
+                                    <div style={{float: 'right', marginTop: '-8px'}}>
+                                        <TextField field={{
+                                            defaultValue: this.props.optionsSelectedById[optionId].comments,
+                                            label: "Option-specific comments",
+                                            name: "options." + optionId + ".comments",
+                                            onChange: this.props.handleCommentsChange,
+                                            section: false,
+                                        }} />
+                                    </div>
+                                :""}
+                                {(this.props.showCommentsTextPerOption)?
+                                    <div style={{float: 'right', width: '50%', minWidth: '200px', display: 'table', height: itemHeight, tableLayout: 'fixed', textAlign: 'right'}}>
+                                        <div style={{display: 'table-cell', verticalAlign: 'middle', overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap'}}>
+                                            {this.props.optionsSelectedById[optionId].comments}
+                                        </div>
+                                    </div>
+                                :""}
+                                
+                                {this.props.hiddenOptionIdField &&
+                                    <HiddenField value={optionId} name={"options." + optionId + ".choices_option_id"} />
                                 }
-                                secondaryText={secondaryText}
-                                secondaryTextLines={1}
-                                style={listItemStyle}
-                            />
-                            <Divider />
-                        </div>
-                    );
+                                
+                                <ListItem
+                                    disabled={typeof(this.props.disabled) !== "undefined"?this.props.disabled:true}
+                                    primaryText={primaryText}
+                                    rightIconButton={
+                                        (this.props.removeButton)?
+                                            <IconButton
+                                                onTouchTap={() => this.props.removeHandler(optionId)}
+                                                iconClassName="material-icons"
+                                            >
+                                                close
+                                            </IconButton>
+                                        :null   //Have to give null as false value, otherwise get warning about using bool when expecting ReactElement
+                                    }
+                                    secondaryText={secondaryText}
+                                    secondaryTextLines={1}
+                                    style={listItemStyle}
+                                />
+                                <Divider />
+                            </div>
+                        );
+                    }
                 }, this)}
             </List>
         );
